90490082103546b2d1d0c5a9a78fca103eea5107
Riya Gupta <riya.gupta@embee.co.in>
2025-05-19 17:16:22 +0530

    fixing configuration issues

--- hrproject/settings.py
+++ hrproject/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hrapp',
-    'channels'
+    'hrapp'
 ]
 
 # Add Channels configuration
